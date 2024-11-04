f6e0a88a5e04720a4fbd1abd66951efada464f4c
Giovane Carvalho <degio2009@hotmail.com>
2024-11-03 22:52:46 -0300

    corrigindo alguns bugs

--- coleta_dados/coleta_dados/spiders/mercadolivre.py
+++ coleta_dados/coleta_dados/spiders/mercadolivre.py
@@ -7,10 +7,16 @@ class MercadolivreSpider(scrapy.Spider):
     start_urls = ["https://lista.mercadolivre.com.br/celular#D[A:celular]"]
 
     def parse(self, response):
-        products = response.css('div.ui-search-result__wrapper')
+        produtos = response.css('li.ui-search-layout__item.shops__layout-item')
 
-        for product in products:
-            yield {
-                'categoria' : product.css('span.poly-component__seller::text').get()
-            }
+        for product in produtos:
+            precos  = product.css('span.andes-money-amount__fraction::text').getall()
 
+
+            yield {'Marca' : product.css('span.poly-component__brand::text').get()
+                ,'Nome'             : product.css('h2.poly-box.poly-component__title::text').get()
+                ,'Vendedor'         : product.css('span.poly-component__seller::text').get()
+                ,'Preco_Antigo'     : precos[0] if len(precos) > 0 else None
+                ,'Preco_Novo'       : precos[1] if len(precos) > 0 else None
+                ,'Perc_Off'         : product.css('span.andes-money-amount__discount::text').get()}
+        
